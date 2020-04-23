e3739218f5b42f115a8b9973e72581102b30d8a2
kesavasubash <kesavasubash@gmail.com>
2020-04-23 18:02:22 +0530

    Demo Program

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Pushing the code into a git repository");
+		System.out.println("Kesava Subash");
+
+	}
+
+}
