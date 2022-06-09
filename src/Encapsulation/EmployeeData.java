578b43de63aa50ee188a7805a196782bc834f33d
KesavaThakkella <52579580+KesavaThakkella@users.noreply.github.com>
2022-06-09 08:07:42 +0530

    Update EmployeeData.java

--- src/Encapsulation/EmployeeData.java
+++ src/Encapsulation/EmployeeData.java
@@ -7,6 +7,16 @@ public class EmployeeData {
 	// Encapsulation is also called as data hiding
 	// Private data variables: so that these var cannot be accessed directly
 	// from ouside of class
+	
+	
+	
+There are four types of Java access modifiers:
+
+Private: The access level of a private modifier is only within the class. It cannot be accessed from outside the class.
+Default: default modifier is only within the package. It cannot be accessed from outside the package. If you do not specify any access level, it will be the default.
+Protected: protected modifier is within the package and outside the package through child class. If you do not make the child class, it cannot be accessed from outside the package.
+Public: public modifier is everywhere. It can be accessed from within the class, outside the class, within the package and outside the package.
+There are many non-access modifiers, such as static, abstract, synchronized, native, volatile, transient, etc. 
 
 	private int ssn;
 	private String ename;
